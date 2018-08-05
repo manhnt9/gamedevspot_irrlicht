c0139faa44fe039d355a8ad5365689df87dc4bf2
ManhNT <manhnt.9@outlook.com>
2018-08-05 17:23:43 +0700

    Add hello world tutorial

---
+++ hello_world/hello.cpp
@@ -0,0 +1,43 @@
+#include <irrlicht.h>
+
+using namespace irr;
+
+using namespace core;
+using namespace scene;
+using namespace video;
+using namespace io;
+using namespace gui;
+
+int main() {
+    auto device = createDevice( video::EDT_OPENGL, dimension2d<u32>(800, 600), 32, false, false, false, 0);
+
+    if (!device)
+        return 1;
+
+    device->setWindowCaption(L"Hello World! - Irrlicht Engine Demo");
+
+    auto driver = device->getVideoDriver();
+    auto smgr = device->getSceneManager();
+
+    auto mesh = smgr->getMesh("./sponza.obj");
+    if (!mesh) {
+        device->drop();
+        return 1;
+    }
+    auto node = smgr->addMeshSceneNode(mesh);
+
+    if (node) {
+        node->setMaterialFlag(EMF_LIGHTING, false);
+    }
+
+    smgr->addCameraSceneNodeFPS(nullptr, 100.f, 1.f);
+
+    while(device->run()) {
+        driver->beginScene(true, true, SColor(255,100,101,140));
+        smgr->drawAll();
+        driver->endScene();
+    }
+
+    device->drop();
+    return 0;
+}
